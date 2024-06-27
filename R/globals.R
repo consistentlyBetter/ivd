7b7efa94e504cfeff79d7d9095ae0bf769f02747
Philippe Rast <rast.ph@gmail.com>
2024-06-26 17:18:53 -0700

    Added globals to avoid notes in CMD Check

---
+++ R/globals.R
@@ -0,0 +1,4 @@
+## globals for ivd function
+utils::globalVariables(c('N', 'K', 'Kr', 'X', 'groupid', 'Z', 'S', 'Sr', 'zeta', 'X_scale', 'Z_scale', 'J', 'P', 'sigma_rand', 'L', 'z', 'ss'))
+## globabls for plot.ivd function
+utils::globalVariables(c('pip', 'id', 'WAIC'))
